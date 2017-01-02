6e096e3d2c1ca30469364e8fbac3fd18e1495ab8
myUbuntu <ketao@ubuntu.(none)>
2017-01-02 18:18:27 +0800

    add code

--- algorithms/sort.h
+++ algorithms/sort.h
@@ -3,6 +3,41 @@
 
 namespace  Common{
 
+template <typename T>
+typedef bool (*CompareFunc)(T,T);
+
+//堆排序
+int Parent(int& pos)
+{ 
+	return pos >> 1 ;
+}
+
+int ChildLeft(int& pos)
+{ 
+	return pos << 1;
+}
+
+int ChildRight(int& pos)
+{ 
+	return pos << 1 + 1;
+}
+
+template <typename T>
+bool max_heapify(T *datas, int datalen, int pos, CompareFunc cmd=NULL)
+{ 
+	int left = 
+	return true;
+}
+
+template <typename T> 
+int heap_sort(T *datas, int datalen, CompareFunc cmp = NULL)
+{ 
+	for(int i = 1; i <= datalen; i++)
+	{ 
+	
+	}
+
+}
 
 
 }
